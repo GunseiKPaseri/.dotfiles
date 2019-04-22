2da7c0d679e8cc0abecb004f25af2d141eb0d8d1
GunseiKPaseri <GunseiKPaseri@gmail.com>
2019-04-22 23:57:35 +0900

    Update windows.bat

--- vscode/windows.bat
+++ vscode/windows.bat
@@ -2,9 +2,10 @@
 rem cdを調整してから管理者権限で実行せよ
 
 
+mklink "C:\Users\%USERNAME%\AppData\Roaming\Code\User\settings.json" "%CD%\settings.json"
+
+
 rem 拡張機能のインストール
 for /f %%a in (extension-list) do (
   code --install-extension %%a
 )
-
-mklink "C:\Users\%USERNAME%\AppData\Roaming\Code\User\settings.json" "%CD%\settings.json"
