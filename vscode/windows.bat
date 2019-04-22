877ba7fbd49019bf9a79863f7411a1bd5c8cdff5
GunseiKPaseri <GunseiKPaseri@gmail.com>
2019-04-22 23:50:52 +0900

    Userの除去・settingのみ保存
    怖い

--- vscode/windows.bat
+++ vscode/windows.bat
@@ -1,8 +1,10 @@
 @echo off
-rem User以下の設定ファイルのリンクを生成
-mklink /D "C:\Users\%USERNAME%\AppData\Roaming\Code\User" "%CD%\User"
+rem cdを調整してから管理者権限で実行せよ
+
 
 rem 拡張機能のインストール
 for /f %%a in (extension-list) do (
   code --install-extension %%a
 )
+
+mklink "C:\Users\%USERNAME%\AppData\Roaming\Code\User\settings.json" "%CD%\settings.json"
