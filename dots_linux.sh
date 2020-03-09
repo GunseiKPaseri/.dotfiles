18d3bf08e3e73bcff70c9140c8f1f3ba2e4b47dd
GunseiKPaseri <gunseikpaseri@gmail.com>
2020-03-10 00:55:58 +0900

    add .bashrc

--- dots_linux.sh
+++ dots_linux.sh
@@ -4,3 +4,6 @@ ln -sf ~/someconfig/.vimrc ~/.vimrc
 ln -sf ~/someconfig/.vim ~/.vim
 ln -sf ~/someconfig/.minttyrc ~/.minttyrc
 ln -sf ~/someconfig/vscode/settings.json ~/.config/Code/User/settings.json
+ln -sf ~/someconfig/.bashrc ~/.bashrc
+
+
