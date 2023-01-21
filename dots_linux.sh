09bec4d2ae90623adcb82992c57149460992c028
GunseiKPaseri <GunseiKPaseri@gmail.com>
2023-01-22 00:54:24 +0900

    add setup.sh

--- dots_linux.sh
+++ dots_linux.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
+
 ln -sf ~/someconfig/.emacs.d ~/.emacs.d
 ln -sf ~/someconfig/.vimrc ~/.vimrc
 # confirm delete .vim
 ln -sf ~/someconfig/.vim ~/.vim
 ln -sf ~/someconfig/.minttyrc ~/.minttyrc
 #ln -sf ~/someconfig/vscode/settings.json ~/.config/Code/User/settings.json
-ln -sf ~/someconfig/nerdps1.sh ~/nerdps1.sh
-ln -sf ~/someconfig/.bashrc ~/.bashrc
-
-
+ln -sf ~/someconfig/.bash_profile ~/.bash_profile
