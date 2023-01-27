63406f8077cdddfe3e07b47eda00ae1772559fc6
GunseiKPaseri <GunseiKPaseri@gmail.com>
2023-01-27 19:18:04 +0900

    add docker

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ set -e # stop with a error
 set -u # forbidden undefined vars
 
 # === STEP CONFIG ===
-STEP_COUNT=12
+STEP_COUNT=15
 COUNTER=1
 
 # === COLOR CONFIG ===
@@ -47,7 +47,7 @@ else
 
   set -x
   sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
-  sudo sed -i.bak -r 's@http://(jp\.)?archive\.ubuntu\.com/ubuntu/?@https://linux.yz.yamagata-u.ac.jp/ubuntu/@g' /etc/apt/sources.list
+  sudo sed -i.bak -r 's@http://((jp\|us\|fr)\.)?archive\.ubuntu\.com/ubuntu/?@https://linux.yz.yamagata-u.ac.jp/ubuntu/@g' /etc/apt/sources.list
   { set +x ; } 2>/dev/null
 fi
 
@@ -74,7 +74,7 @@ MSG="Hide needrestart"
 bash -c "which needrestart >/dev/null 2>&1" || EXIST_CMD=$?
 if [ $MODE -le 0 ]; then
   printf "[${COUNTER}/${STEP_COUNT}] SKIP ${MSG} (Need sudo)\n"
-elif [ $EXIST_CMD -ne 0 ]; then
+elif [ $EXIST_CMD -eq 1 ]; then
   printf "[${COUNTER}/${STEP_COUNT}] SKIP ${MSG} (No command needrestart)\n"
 else
   printf "[${COUNTER}/${STEP_COUNT}] ${COLOR_CYAN}${MSG}${COLOR_OFF}\n"
@@ -97,7 +97,7 @@ MSG="INSTALL apt-fast"
 bash -c "which apt-fast >/dev/null 2>&1" || EXIST_CMD=$?
 if [ $MODE -le 0 ]; then
   printf "[${COUNTER}/${STEP_COUNT}] SKIP ${MSG} (Need sudo)\n"
-elif [ $EXIST_CMD -eq 0 ]; then
+elif [ $EXIST_CMD -ne 1 ]; then
   printf "[${COUNTER}/${STEP_COUNT}] SKIP ${MSG} (installed)\n"
 else
   printf "[${COUNTER}/${STEP_COUNT}] ${COLOR_CYAN}${MSG}${COLOR_OFF}\n"
@@ -122,7 +122,10 @@ else
   case "${YN_SELECTOR}" in
     [Yy]* )
       set -x
-      sudo apt-fast install -y language-pack-ja manpages-ja manpages-ja-dev
+      sudo apt-fast install -y \
+        language-pack-ja \
+        manpages-ja \
+        manpages-ja-dev
       sudo update-locale LANG=ja_JP.UTF-8
       { set +x ; } 2>/dev/null
       ;;
@@ -139,7 +142,7 @@ else
   printf "[${COUNTER}/${STEP_COUNT}] ${COLOR_CYAN}${MSG}${COLOR_OFF}\n"
 
   bash -c "which pip3 >/dev/null 2>&1" || EXIST_CMD=$?
-  if [ $EXIST_CMD -ne 0 ]; then
+  if [ $EXIST_CMD -eq 1 ]; then
     sudo apt-fast install -y python3-pip
     pip3 install --upgrade pip
     export PATH="$PATH:$HOME/.local/bin"
@@ -198,7 +201,7 @@ COUNTER=`expr $COUNTER + 1`
 MSG="CLONE dotfiles.git"
 
 bash -c "which git >/dev/null 2>&1" || EXIST_CMD=$?
-if [ $EXIST_CMD -ne 0 ]; then
+if [ $EXIST_CMD -eq 1 ]; then
   printf "[${COUNTER}/${STEP_COUNT}] SKIP ${MSG} (need git command)\n"
 else
   printf "[${COUNTER}/${STEP_COUNT}] ${COLOR_CYAN}${MSG}${COLOR_OFF}\n"
@@ -217,12 +220,12 @@ MSG="INSTALL vim"
 bash -c "which vim >/dev/null 2>&1" || EXIST_CMD=$?
 if [ $MODE -le 0 ]; then
   printf "[${COUNTER}/${STEP_COUNT}] SKIP ${MSG} (Need sudo)\n"
-elif [ $EXIST_CMD -eq 0 ]; then
+elif [ $EXIST_CMD -eq 1 ]; then
   printf "[${COUNTER}/${STEP_COUNT}] SKIP ${MSG} (installed)\n"
 else
   printf "[${COUNTER}/${STEP_COUNT}] ${COLOR_CYAN}${MSG}${COLOR_OFF}\n"
   bash -c "which gnome-shell >/dev/null 2>&1" || EXIST_CMD=$?
-  if [ EXIST_CMD -eq 0 ]; then
+  if [ $EXIST_CMD -ne 1 ]; then
     set -x
     sudo apt-fast install -y vim-gnome
     { set +x ; } 2>/dev/null
@@ -240,7 +243,7 @@ MSG="INSTALL tmux"
 bash -c "which tmux >/dev/null 2>&1" || EXIST_CMD=$?
 if [ $MODE -le 0 ]; then
   printf "[${COUNTER}/${STEP_COUNT}] SKIP ${MSG} (Need sudo)\n"
-elif [ $EXIST_CMD -eq 0 ]; then
+elif [ $EXIST_CMD -ne 1 ]; then
   printf "[${COUNTER}/${STEP_COUNT}] SKIP ${MSG} (installed)\n"
 else
   printf "[${COUNTER}/${STEP_COUNT}] ${COLOR_CYAN}${MSG}${COLOR_OFF}\n"
@@ -256,7 +259,7 @@ MSG="INSTALL fish"
 bash -c "which fish >/dev/null 2>&1" || EXIST_CMD=$?
 if [ $MODE -le 0 ]; then
   printf "[${COUNTER}/${STEP_COUNT}] SKIP ${MSG} (Need sudo)\n"
-elif [ $EXIST_CMD -eq 0 ]; then
+elif [ $EXIST_CMD -ne 1 ]; then
   printf "[${COUNTER}/${STEP_COUNT}] SKIP ${MSG} (installed)\n"
 else
   printf "[${COUNTER}/${STEP_COUNT}] ${COLOR_CYAN}${MSG}${COLOR_OFF}\n"
@@ -264,6 +267,57 @@ else
   sudo add-apt-repository ppa:fish-shell/release-3 -y
   sudo apt-fast update
   sudo apt-fast install -y fish
+  { set +x ; } 2>/dev/null
+fi
+
+COUNTER=`expr $COUNTER + 1`
+
+# === INSTALL asdf ===
+
+MSG="INSTALL asdf"
+bash -c "which asdf >/dev/null 2>&1" || EXIST_CMD=$?
+if [ $MODE -le 0 ]; then
+  printf "[${COUNTER}/${STEP_COUNT}] SKIP ${MSG} (Need sudo)\n"
+elif [ $EXIST_CMD -ne 1 ]; then
+  printf "[${COUNTER}/${STEP_COUNT}] SKIP ${MSG} (installed)\n"
+else
+  printf "[${COUNTER}/${STEP_COUNT}] ${COLOR_CYAN}${MSG}${COLOR_OFF}\n"
+  set -x
+
+  # skip
+
+  { set +x ; } 2>/dev/null
+fi
+
+COUNTER=`expr $COUNTER + 1`
+
+# === INSTALL docker ===
+
+MSG="INSTALL docker"
+bash -c "which docker >/dev/null 2>&1" || EXIST_CMD=$?
+if [ $MODE -le 0 ]; then
+  printf "[${COUNTER}/${STEP_COUNT}] SKIP ${MSG} (Need sudo)\n"
+elif [ $EXIST_CMD -ne 1 ]; then
+  printf "[${COUNTER}/${STEP_COUNT}] SKIP ${MSG} (installed)\n"
+else
+  printf "[${COUNTER}/${STEP_COUNT}] ${COLOR_CYAN}${MSG}${COLOR_OFF}\n"
+  set -x
+
+  sudo apt-fast install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.1
+  . "$HOME/.asdf/asdf.sh"
+  echo ". \"$$HOME/.asdf/completions/asdf.bash\"" > ~/.bashrc
+
   { set +x ; } 2>/dev/null
 fi
 
@@ -279,5 +333,16 @@ curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fi
 
 COUNTER=`expr $COUNTER + 1`
 
+# === CLEAN STEP ===
+MSG="clean"
+if [ $MODE -le 0 ]; then
+  printf "[${COUNTER}/${STEP_COUNT}] SKIP ${MSG} (Need sudo)\n"
+else
+  printf "[${COUNTER}/${STEP_COUNT}] ${COLOR_CYAN}${MSG}${COLOR_OFF}\n"
+
+  set -x
+  sudo apt autoremove
+  { set +x ; } 2>/dev/null
+fi
 # === set thisfile ===
 source ~/.bash_profile
